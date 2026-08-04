feec9cf73ced2f4398d74b4bd92079f12fd93f42
indyteo <theo.mail@laposte.net>
2026-08-04 17:28:17 +0200

    chore(init): Initialize module

---
+++ src/main/java/fr/theoszanto/minalac/mcschem/MinecraftSchematicModule.java
@@ -0,0 +1,10 @@
+package fr.theoszanto.minalac.mcschem;
+
+import com.ignfab.minalac.generator.utils.modules.Module;
+
+/**
+ * Minecraft schematic (Sponge Schematic version 3) module.
+ * @see <a href="https://github.com/SpongePowered/Schematic-Specification/blob/master/versions/schematic-3.md">Sponge Schematic Specification version 3</a>
+ */
+public class MinecraftSchematicModule extends Module {
+}
